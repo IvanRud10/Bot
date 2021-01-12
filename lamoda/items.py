90aa609a859003123c31f3b8f2262db89e784126
Ivan <72566481+IvanRud10@users.noreply.github.com>
2021-01-12 19:50:47 +0200

    Update items.py

--- lamoda/items.py
+++ lamoda/items.py
@@ -2,24 +2,7 @@ from scrapy.item import Item, Field
 
 
 class PropertiesItem(Item):
-    # Primary fields
     title = Field()
-    price = Field()
-    description = Field()
-    address = Field()
-    image_urls = Field()
-
-    # Calculated fields
-    images = Field()
-    location = Field()
-
-    # Housekeeping fields
-    url = Field()
-    project = Field()
-    spider = Field()
-    server = Field()
-    date = Field()
-
     category = Field()
     article = Field()
     first_price = Field()
